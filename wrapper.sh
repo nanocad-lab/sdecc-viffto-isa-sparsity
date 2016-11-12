bb77ba99e2b02e41eb2a3f89ede2f294ea0b6c88
Mark Gottscho <mgottscho@ucla.edu>
2016-11-11 19:55:08 -0800

    Added wrapper script

---
+++ wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BINARY_LOCATION=$PWD
+
+if [[ "$MWG_MACHINE_NAME" == "hoffman" ]]; then
+    MY_PRELOAD=$GCC5/lib64/libstdc++.so.6
+elif [[ "$MWG_MACHINE_NAME" == "nanocad-server-testbed" ]]; then
+    MY_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+elif [[ "$MWG_MACHINE_NAME" == "dfm" ]]; then
+    MY_PRELOAD=/usr/lib64/libstdc++.so.6 # Not working as of 9/26/2016
+fi
+
+LD_PRELOAD=$MY_PRELOAD LD_LIBRARY_PATH=$MCRROOT/bin/glnxa64:$MCRROOT/runtime/glnxa64:$LD_LIBRARY_PATH $BINARY_LOCATION/rv64g_sparsity_check
