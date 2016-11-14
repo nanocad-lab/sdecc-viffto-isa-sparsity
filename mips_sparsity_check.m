2ddc518b9448ee0ee2548a111cfc8dd03d07d62c
Mark Gottscho <mgottscho@ucla.edu>
2016-11-13 23:53:52 -0800

    Added sparsity check code for MIPS and Alpha.

---
+++ mips_sparsity_check.m
@@ -0,0 +1,29 @@
+function [fraction_illegal_inst] = mips_sparsity_check(n_threads)
+
+n_threads = str2num(n_threads)
+%% Set up parallel computing
+pctconfig('preservejobs', true);
+mycluster = parcluster('local');
+mycluster.NumWorkers = n_threads;
+mypool = parpool(mycluster,n_threads);
+work_per_thread = cast((2^32),'uint64')/n_threads;
+
+% Try all possible 32-bit patterns
+legal_count = zeros(n_threads,1);
+parfor i=1:n_threads
+    offset = (i-1)*work_per_thread;
+    for j=1:work_per_thread
+        bin = dec2bin(offset+j,32);
+        hex = my_bin2hex(bin);
+        [legal] = MyMipsDecoder(hex);
+        if legal == 1
+            legal_count(i) = legal_count(i)+1;
+        end
+    end
+    display(['thread ' num2str(i) ' is done']);
+end
+
+total_legal_count = sum(legal_count);
+fraction_illegal_inst = total_legal_count/(2^32)
+
+end
--- rv64g_sparsity_check.cmd
+++ rv64g_sparsity_check.cmd
@@ -7,12 +7,12 @@
 #  Use current working directory
 #$ -cwd
 #  input           = /dev/null
-#  output          = /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID
-#$ -o /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID
+#  output          = /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID
+#$ -o /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID
 #  error           = Merged with joblog
 #$ -j y
 #  The following items pertain to the user program
-#  user program    = /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.m
+#  user program    = /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.m
 #  arguments       = -m
 #  program input   = Specified by user program
 #  program output  = Specified by user program
@@ -34,17 +34,17 @@
   unalias *
   set qqversion = 
   set qqapp     = "mcc serial"
-  set qqidir    = /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity
+  set qqidir    = /u/project/puneet/mgottsch/eccgrp-isa-sparsity
   set qqjob     = rv64g_sparsity_check
-  set qqodir    = /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity
-  cd     /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity
+  set qqodir    = /u/project/puneet/mgottsch/eccgrp-isa-sparsity
+  cd     /u/project/puneet/mgottsch/eccgrp-isa-sparsity
   source /u/local/bin/qq.sge/qr.runtime
   if ($status != 0) exit (1)
 #
   echo "UGE job for rv64g_sparsity_check built Fri Nov 11 19:00:37 PST 2016"
   echo ""
   echo "  rv64g_sparsity_check directory:"
-  echo "    "/u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity
+  echo "    "/u/project/puneet/mgottsch/eccgrp-isa-sparsity
   echo "  Submitted to UGE:"
   echo "    "$qqsubmit
   echo "  SCRATCH directory:"
@@ -67,17 +67,17 @@ requeue:
   /u/local/apps/matlab/8.6/bin/mcc rv64g_sparsity_check.m -m
   set rc = $status
 #
-# if( `grep -c 'Maximum number of users' /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 || `grep -c 'License checkout failed' /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 ) then
-  if( `grep -c 'Maximum number of users' /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 || `grep -c 'License checkout failed' /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 || `grep -c 'Could not check out a Compiler license' /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 ) then
-    head -n 13 /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID > /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/.$$
-    echo "------------ waiting for a license. retrying mcc command." >> /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/.$$
+# if( `grep -c 'Maximum number of users' /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 || `grep -c 'License checkout failed' /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 ) then
+  if( `grep -c 'Maximum number of users' /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 || `grep -c 'License checkout failed' /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 || `grep -c 'Could not check out a Compiler license' /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID` > 0 ) then
+    head -n 13 /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID > /u/project/puneet/mgottsch/eccgrp-isa-sparsity/.$$
+    echo "------------ waiting for a license. retrying mcc command." >> /u/project/puneet/mgottsch/eccgrp-isa-sparsity/.$$
     sleep 90
-    mv /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/.$$ /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID
+    mv /u/project/puneet/mgottsch/eccgrp-isa-sparsity/.$$ /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID
     goto requeue
   endif
 #
   echo ""
-  if( $rc != 0 || ! -e /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check ) then
+  if( $rc != 0 || ! -e /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check ) then
     echo ============================================================
     echo ERROR: mcc.queue failed with status $rc
     echo ============================================================
@@ -90,12 +90,12 @@ requeue:
 #
   source /u/local/bin/qq.sge/qr.runtime
 #
-  echo "-------- /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID --------" >> /u/local/apps/queue.logs/mcc.log.serial
- if (`wc -l /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID  | awk '{print $1}'` >= 1000) then
-        head -50 /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID >> /u/local/apps/queue.logs/mcc.log.serial
+  echo "-------- /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID --------" >> /u/local/apps/queue.logs/mcc.log.serial
+ if (`wc -l /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID  | awk '{print $1}'` >= 1000) then
+        head -50 /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID >> /u/local/apps/queue.logs/mcc.log.serial
         echo " "  >> /u/local/apps/queue.logs/mcc.log.serial
-        tail -10 /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID >> /u/local/apps/queue.logs/mcc.log.serial
+        tail -10 /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID >> /u/local/apps/queue.logs/mcc.log.serial
   else
-        cat /u/project/puneet/mgottsch/eccgrp-riscv-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID >> /u/local/apps/queue.logs/mcc.log.serial
+        cat /u/project/puneet/mgottsch/eccgrp-isa-sparsity/rv64g_sparsity_check.joblog.$JOB_ID >> /u/local/apps/queue.logs/mcc.log.serial
   endif
   exit (0)
